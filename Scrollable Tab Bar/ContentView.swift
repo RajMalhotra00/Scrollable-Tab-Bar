718c9fd4004482c8d88306290a8724948cba15ed
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-25 15:05:54 +0200

    Initial Commit

---
+++ Scrollable Tab Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrollable Tab Bar
+//
+//  Created by Raj Malhotra on 25.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrollable Tab Bar/Scrollable_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrollable_Tab_BarApp.swift
+//  Scrollable Tab Bar
+//
+//  Created by Raj Malhotra on 25.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Scrollable_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
